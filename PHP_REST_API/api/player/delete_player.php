29b743b67b4fc3135648f526e7b166903bcb3e99
mikeApostolidis <mike23apostolidis@gmail.com>
2022-12-07 11:02:21 +0200

    added delete player function

---
+++ PHP_REST_API/api/player/delete_player.php
@@ -0,0 +1,43 @@
+<?php
+   //headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Player.php';
+
+   //instantiate db & connect
+    $database = new Database();
+    $db = $database->connect();
+
+   //instantiate blog post object
+    $player = new Player($db);
+
+    //get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    //set id to delete
+    $player->setId($data->id);
+   // $player->id = $data->id;
+   // $player->setName($data->name);
+
+
+   //delete post
+   if($player->delete_player()){
+    echo json_encode(
+        array('message' => 'Player Deleted')
+    );
+    }else{
+         echo json_encode(
+           array('message' => 'Player Not Deleted')
+         );
+    }
+  
+
+
+
