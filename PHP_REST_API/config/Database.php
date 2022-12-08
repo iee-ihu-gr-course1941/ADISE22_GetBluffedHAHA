ed568ccaf68591a9d48549e96d91acfb7aaaf0fa
Alex Kaloudis <kaloudisalexis@gmail.com>
2022-12-08 13:08:28 +0200

    database fix

--- PHP_REST_API/config/Database.php
+++ PHP_REST_API/config/Database.php
@@ -2,22 +2,22 @@
 class Database{
     //DB parameters
     // private $host = 'localhost';
-    private $host = '/home/student/it/2018/it185186/mysql/run/mysql.sock23';
+    private $host = 'users.iee.ihu.gr';
     private $db = 'bluff_db';
     private $user = 'root';
     // private $pass = 'root'; //mikes password
-    // private $pass = 'adminalex';//alex localhost
-     private $pass = 'thisismydatabasepass';//alex users.iee.ihu.gr   
+    private $pass = 'thisismydatabasepass';  
     private $conn;
-    
+    // private $instanceUnixSocket = getenv('/home/student/it/2018/it185186/mysql/run/mysql.sock');
 
     public function connect(){
         $this->conn = null;
 
         try{
-            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db,
-             $this->user, $this->pass);  
-             $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);  
+            $mysqli = new mysqli($this->host, $this->user,$this->pass, $this->db,null,'/home/student/it/2018/it185186/mysql/run/mysql.sock');
+            // $this->conn = new PDO('mysql:host='.$this->dsn.';dbname='.$this->db,
+            //  $this->user, $this->pass);  
+            //  $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);  
         }catch(PDOException $e){
             echo 'connection failed: '.$e->getMessage();      
         }
