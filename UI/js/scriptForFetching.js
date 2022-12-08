b2883b2d013c91cd5cdd7cf37295d98a3a2d0d65
Alex Kaloudis <kaloudisalexis@gmail.com>
2022-12-08 11:27:06 +0200

    adjusted url for fetching data from the API

--- UI/js/scriptForFetching.js
+++ UI/js/scriptForFetching.js
@@ -114,8 +114,10 @@
             e.preventDefault();
 
             let name = document.getElementById('playername').value;
+            const url = 'https://users.it.teithe.gr/PHP_REST_API/api/player/add_new_player.php';
+            // const url = 'http://localhost/PHP_REST_API/api/player/add_new_player.php';
             if(name.length != 0 && name != ""){
-                fetch('http://localhost/PHP_REST_API/api/player/add_new_player.php', {
+                fetch(url, {
                 method:'POST',
                 headers: {
                 'Accept': 'application/json, text/plain, */*',
