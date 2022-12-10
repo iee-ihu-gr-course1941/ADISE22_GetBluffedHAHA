a24b4f81de91263b08626155070dd50bff6fdef5
Alex Kaloudis <kaloudisalexis@gmail.com>
2022-12-10 13:07:16 +0200

    try new connect file to users

---
+++ PHP_REST_API/config/UsersConnect.php
@@ -0,0 +1,19 @@
+<?php
+$host='localhost';
+$db = 'chess';
+
+$user='root';
+$pass='thisismydatabasepass';
+
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2018/it185186/mysql/run/mysql.sock');
+} else {
+		$pass=null;
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
