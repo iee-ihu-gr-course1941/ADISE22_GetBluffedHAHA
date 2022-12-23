d90844c027216c4c082cb82ae4e4cdddc6c622d4
mikeApostolidis <mike23apostolidis@gmail.com>
2022-12-23 18:45:36 +0200

    highlight cards
    implemented highlighting cards by clicking them

--- css-deck-of-cards/script.js
+++ css-deck-of-cards/script.js
@@ -1,14 +1,16 @@
-const cards = document.querySelectorAll(".card")
 
+const cards = document.querySelectorAll(".card")
+console.log(cards);
 cards.forEach(addCardElements)
 
 function addCardElements(card) {
   const value = card.dataset.value
-  // const suit = card.dataset.suit
-
+  //const suit = card.dataset.suit
+  
   const valueAsNumber = parseInt(value)
   if (isNaN(valueAsNumber)) {
     card.append(createPip())
+    
   } else {
     for (let i = 0; i < valueAsNumber; i++) {
       card.append(createPip())
@@ -21,7 +23,9 @@ function addCardElements(card) {
 
 function createCornerNumber(position, value) {
   const corner = document.createElement("div")
+  
   corner.textContent = value
+  //console.log(corner);
   corner.classList.add("corner-number")
   corner.classList.add(position)
   return corner
@@ -29,6 +33,37 @@ function createCornerNumber(position, value) {
 
 function createPip() {
   const pip = document.createElement("div")
+ // console.log(pip);
   pip.classList.add("pip")
   return pip
 }
+
+
+//highlight card by clicking it and remove by clicking it again
+var elements = document.getElementsByClassName("card");
+
+var myFunction = function(){  
+ if(this.getAttribute("class") == "card") {
+  this.setAttribute("class","clicked");
+}else{
+  var attribute = this.setAttribute("class","card"); 
+}
+};
+
+
+for(var i=0;i<elements.length;i++){     
+      elements[i].addEventListener('click',myFunction, false);           
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
