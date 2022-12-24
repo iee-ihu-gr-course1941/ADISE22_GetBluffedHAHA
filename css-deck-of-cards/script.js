863722e57b99781df142687b08edba157a0097c8
mikeApostolidis <mike23apostolidis@gmail.com>
2022-12-24 12:03:57 +0200

    tried to implement drag and drop
    drag and drop cards to board(div).Needs more work

--- css-deck-of-cards/script.js
+++ css-deck-of-cards/script.js
@@ -50,10 +50,78 @@ var myFunction = function(){
 }
 };
 
-
 for(var i=0;i<elements.length;i++){     
-      elements[i].addEventListener('click',myFunction, false);           
+      elements[i].addEventListener('click',myFunction, false); 
+      elements[i].addEventListener('dragstart',myFunction, false); //drag n drop
+      elements[i].addEventListener('dragend',myFunction, false); //drag n drop
+            
 }
+
+
+//drag n drop begins
+document.addEventListener('DOMContentLoaded', (event) => {
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+
+  
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+ // e.dataTransfer.setData('text/html', this.innerHTML)
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+
+  e.dataTransfer.dropEffect = 'move';
+  
+  return false;
+}
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+// function handleDrop(e) {
+//   if (e.stopPropagation) {
+//     e.stopPropagation(); // stops the browser from redirecting.
+//   }
+  
+//   if (dragSrcEl != this) {
+//     dragSrcEl.innerHTML = this.innerHTML;
+//     this.innerHTML = e.dataTransfer.getData('text/html');
+    
+//   }
+  
+//   return false;
+// }
+
+function handleDragEnd(e) {
+  this.style.opacity = '1';
+  
+  items.forEach(function (item) {
+    item.classList.remove('over');
+  });
+}
+
+let items = document.querySelectorAll('.card');
+ items.forEach(function (item) {
+  item.addEventListener('dragstart', handleDragStart);
+  item.addEventListener('dragover', handleDragOver);
+  item.addEventListener('dragenter', handleDragEnter);
+  item.addEventListener('dragleave', handleDragLeave);
+  item.addEventListener('dragend', handleDragEnd);
+  item.addEventListener('drop', handleDrop);
+});
+});
+//drag n drops ends
+
 
 
 
